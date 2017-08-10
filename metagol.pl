69f631289d00a68e375df2d4953b4408eaf43a1f
Andrew Cropper <andrew@blueberry.home>
2017-08-10 17:07:57 +0100

    reverted code that prints programs back to the original code where clauses are printed top to bottom

--- metagol.pl
+++ metagol.pl
@@ -149,9 +149,8 @@ invented_symbols(MaxClauses,P/A,[sym(P,A,_U)|Sig]):-
 
 pprint(Prog1):-
   map_list_to_pairs(arg(2),Prog1,Pairs),
-  % keysort(Pairs,Sorted),
-  % pairs_values(Sorted,Prog2),
-  pairs_values(Pairs,Prog2),
+  keysort(Pairs,Sorted),
+  pairs_values(Sorted,Prog2),
   maplist(pprint_clause,Prog2).
 
 pprint_clause(Sub):-
