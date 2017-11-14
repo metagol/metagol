11b568b9313c7a93e472e6f94afdb15ba8643f6b
Andrew Cropper <andrew.cropper@gmail.com>
2017-11-14 08:35:31 +0000

    tidied up prove prim predicate

--- metagol.pl
+++ metagol.pl
@@ -78,7 +78,8 @@ prove_aux('@'(Atom)-_,_FullSig,_Sig,_MaxN,N,N,Prog,Prog):-!,
 
 %% prove primitive atom
 prove_aux([P|Args]-_Path,_FullSig,_Sig,_MaxN,N,N,Prog,Prog):-
-    (nonvar(P)-> (user:prim(P/_),!,user:primcall(P,Args)); user:primcall(P,Args)).
+    (nonvar(P)-> (user:prim(P/_),!); true),
+    user:primcall(P,Args).
 
 %% use interpreted BK
 prove_aux(Atom-Path,FullSig,Sig,MaxN,N1,N2,Prog1,Prog2):-
